2afc32a3662887f27add9917eaf243bf90586001
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-12-05 11:05:22 +0100

    create-task-5

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const bgColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click" , () => {
+  const result = (document.body.style.backgroundColor = getRandomHexColor());
+  bgColor.textContent = result;
+});
+
