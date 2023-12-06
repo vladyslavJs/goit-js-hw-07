614d005c44537826d8fdd64f302314b02175cbd9
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-12-06 12:11:33 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,19 @@
+const logForm = document.querySelector(".login-form");
 
+logForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const email = logForm.elements.email.value;
+    const password = logForm.elements.password.value;
+
+    if (email === "" || password === "") {
+        return alert("All form fields must be filled in");
+    }
+
+    const information = {
+        email: email.trim(),
+        password: password.trim(),
+    };
+
+    console.log(information);
+    logForm.reset();
+});
